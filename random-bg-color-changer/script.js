f8788bc13421b5f196dc51fe0eea310ec7309abd
Syed Muhammad Zaid <95569344+zaidsyed12@users.noreply.github.com>
2024-07-19 12:40:30 +0500

    Add files via upload

---
+++ random-bg-color-changer/script.js
@@ -0,0 +1,34 @@
+const randomColor = function(){
+    const hexRange = '0123456789ABCDEF';
+    let color = '#';
+
+    for(let i=0; i<6; i++){
+        color+=hexRange[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+};
+
+let intervalId
+
+const startChangingColor = function(){
+
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+
+   function changeBgColor(){
+    document.body.style.backgroundColor = randomColor();
+   }
+
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
